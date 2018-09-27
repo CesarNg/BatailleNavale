c1df571c93e550bbe29c28356360d6743af165c1
Julien-ABT <abtjulien@gmail.com>
2018-09-27 21:06:23 +0200

    Modification bataille, ajout des tours de jeu et de la condition de victoire

--- src/main/java/game/Bataille.java
+++ src/main/java/game/Bataille.java
@@ -4,11 +4,6 @@ import exception.SaisieErroneeException;
 
 public class Bataille {
 
-    private Torpilleur torpilleur;
-    private ContreTorpilleur contreTorpilleur;
-    private PorteAvion porteAvion;
-    private SousMarin sousMarin;
-    private Croiseur  croiseur;
     private String [][] champBataille;
     private Joueur joueur1;
     private Joueur joueur2;
@@ -19,20 +14,48 @@ public class Bataille {
 
 
     public Bataille(){
+        boolean fin =false;;
 
-         joueur1 = new Joueur("Cesar");
+        joueur1 = new Joueur("Cesar");
         try {
             interfaceJeu1 = new InterfaceJeu(joueur1);
         } catch (SaisieErroneeException e) {
             e.printStackTrace();
         }
 
-        joueur1.getListBateau().get(0).deplacement(6,2);
+        //joueur1.getListBateau().get(0).deplacement(6,2);
 
-         interfaceJeu1.updateGrid(joueur1);
+        //interfaceJeu1.updateGrid(joueur1);
 
-        // joueur2 = new Joueur("Brice");
-        // interfaceJeu2 = new InterfaceJeu(joueur2);
+        joueur2 = new Joueur("Brice");
+        try {
+            interfaceJeu2 = new InterfaceJeu(joueur2);
+        } catch (SaisieErroneeException e) {
+            e.printStackTrace();
+        }
+
+        while(!fin){
+            Boolean j1Touche=false,j2Touche=false;
+
+            if (!joueur1.Defaite()&&!joueur2.Defaite()) {
+                System.out.println("Tour du joueur 1");
+                interfaceJeu1.updateGrid(joueur1);
+                j2Touche = joueur1.tir(joueur2);
+            }else fin=true;
+
+            if (!joueur1.Defaite()&&!joueur2.Defaite()) {
+                System.out.println("Tour du joueur 2");
+                interfaceJeu1.updateGrid(joueur2);
+                j1Touche = joueur2.tir(joueur1);
+            }else fin=true;
+
+        }
+
+        if (joueur1.Defaite()){
+            System.out.println("Joueur 2 à gangé! Bravo "+joueur2.getNom());
+        }else{
+            System.out.println("Joueur 1 à gangé! Bravo "+joueur1.getNom());
+        }
 
     }
 
